438ff3b1c39f6ee40c98fe623088d7e811636776
John <johntheherrera@gmail.com>
2025-09-25 22:16:26 -0700

    Made changes to FreeRTOS

--- software/main/src/main.cpp
+++ software/main/src/main.cpp
@@ -1,17 +1,33 @@
-#include <Arduino.h>
+#include "arduino_freertos.h"
 #include "defines.h"
 #include "motor.h"
 
 using namespace std;
 
+// Globals
+// FIXME put somewhere else
+TaskHandle_t testTaskHandle = NULL;
+
+void testTask(void *pvParameters)
+{
+    while (1)
+    {
+        printf("I am a task");
+    }
+}
+
 void setup()
 {
-    myFunction(1, 2);
+    // Task Creation
+    xTaskCreate(testTask, "Test Task", 1000, NULL, 1, &testTaskHandle);
+
+    vTaskStartScheduler();
 }
 
 void loop()
 {
     // Do Stuff
     cout << "I am alive! \n";
-
+    myFunction(1, 2);
 }
+
