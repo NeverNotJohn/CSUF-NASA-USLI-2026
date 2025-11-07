fc733be5c1a33aa7242392636503a313c1613a7c
Birb <51867136+Birb-jpg@users.noreply.github.com>
2025-11-07 00:02:27 -0800

    Update bmp280.h

--- payload/software/main/include/bmp280.h
+++ payload/software/main/include/bmp280.h
@@ -22,5 +22,5 @@ float getTemperature();
 // Reads the altitude data using the baseline pressure
 float getRelativeAltitude();
 // Really for debugging
-float getBaselinePressure();
+double getBaselinePressure();
 #endif // BMP280_H
