35a5878e573e742db824c1a7b75b7c9807e6192f
Isaac L <isaac.lay25@gmail.com>
2025-11-08 19:39:24 -0800

    complementary filter
    updated "orientationCalculation()".
    - mutates a vec3 in place.
    - complementary filter with accelerometer data

--- payload/software/main/src/main.cpp
+++ payload/software/main/src/main.cpp
@@ -46,22 +46,48 @@ void testMPU(void *pvParameters)
         Serial.println(" (rad/s)");
     }
 }
+/**
+ * @brief Calculates the orientation using angular velocity and acceleration over a set delta time (to be handled by the loop).
+ * This function takes four parameters and calculates Roll, Pitch, and Yaw.
+ * 
+ * @param gyroData A Vector3D with angular velocity data
+ * @param accelData A Vector3D with acceleration data
+ * @param orientation Reference to a Vector3D that will be mutated in place
+ * @param deltaTime Difference in time.
+ * @param accelMix Weight for gyro vs accel (0.96-0.98 typical).
+ */
+void orientationCalculation(Vector3D gyroData, Vector3D accelData, Vector3D& orientation, double deltaTime, double accelMix = 0.98)
+{
+    // Integrate angular velocity to get orientation with respect to time
+    // Very prone to drift but proof of concept!!
 
-void orientationLoop(void *pvParameters){
+    orientation.x += gyroData.x * deltaTime; // Roll 
+    orientation.y += gyroData.y * deltaTime; // Pitch
+    orientation.z += gyroData.z * deltaTime; // Yaw
+    
+    // Calculate roll and pitch from the accelerometer
+    float accelRoll = atan2(accelData.y, accelData.z);
+    float accelPitch = atan2(-accelData.x, sqrt(accelData.y * accelData.y + accelData.z * accelData.z));
+    
+    // Apply complementary filter that blends in the gyro intergration with accel data
+    // Greater accuracy for roll and pitch, sadly no yaw correction
+    // Slowly will drift to the right orientation
+    orientation.x  = accelMix * orientation.x + (1.0 - accelMix) * accelRoll;
+    orientation.y = accelMix * orientation.y + (1.0 - accelMix) * accelPitch;
+}
+
+void orientationLoop(void *pvParameters)
+{
     // freeRTOS notated infinite loop 
     TickType_t xLastWakeTime = xTaskGetTickCount(); // Get current time in ticks
     const TickType_t xFrequency = pdMS_TO_TICKS(10); // 100Hz/10MS
     const double deltaTime = 0.01; // 0.01s = 10ms
-    Vector3D orientation = {0, 0, 0};
+    Vector3D orientation = {0, 0, 0}; // Init this out side of the loop
     for(;;){
         Vector3D angular = getAngularVelocity();
-        // Integrate angular velocity to get orientation with respect to time
-        // Very prone to drift but proof of concept!!
-
-        orientation.x += angular.x * deltaTime; // Roll 
-        orientation.y += angular.y * deltaTime; // Pitch
-        orientation.z += angular.z * deltaTime; // Yaw
-
+        Vector3D acceleration = getAcceleration();
+        orientationCalculation(angular,acceleration,orientation,deltaTime);
+        // Convert the orientation into degrees btw
         Serial.print("Orientation - ");
         Serial.print("x (Roll): ");
         Serial.print(orientation.x * 180.0 / PI);
