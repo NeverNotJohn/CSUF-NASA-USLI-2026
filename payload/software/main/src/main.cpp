4e5d0534fc089b311d2416fc4169145353c7843a
Isaac Lay <isaaclay.25@gmail.com>
2025-11-20 12:55:51 -0800

    NEO6M Intergration
    added
    functions to init and get values using the tinygpsplus library

--- payload/software/main/src/main.cpp
+++ payload/software/main/src/main.cpp
@@ -1,9 +1,7 @@
 #include "arduino_freertos.h"
 #include "defines.h"
 #include "motor.h"
-#include "mpu6050.h"
-#include "bmp280.h"
-#include <MadgwickAHRS.h>
+#include "neo6m.h"
 using namespace std;
 
 // Globals
@@ -11,8 +9,7 @@ using namespace std;
 TaskHandle_t testTaskHandle = NULL;
 TaskHandle_t bmpTestHandle = NULL;
 TaskHandle_t orientationLoopHandle = NULL;
-SemaphoreHandle_t i2cMutex = NULL;
-Madgwick filterMadgwick;
+
 
 void testTask(void *pvParameters)
 {
@@ -26,69 +23,27 @@ void testTask(void *pvParameters)
 }
 
 
-void orientationLoop(void *pvParameters)
-{
-    TickType_t xLastWakeTime = xTaskGetTickCount(); // Get current time in ticks
-    const TickType_t xFrequency = pdMS_TO_TICKS(10); // 100Hz/10ms
-    const double deltaTime = pdTICKS_TO_MS(xFrequency)/1000.0; // auto converts ticks to seconds for delta time
-        // freeRTOS notated infinite loop 
-    for(;;)
-    {
-        Vector3D angular = {0, 0, 0};
-        Vector3D acceleration = {0, 0, 0};
-        if(xSemaphoreTake(i2cMutex, portMAX_DELAY) == pdTRUE)
-        {
-            angular = getAngularVelocity();
-            acceleration = getAcceleration();
-            xSemaphoreGive(i2cMutex);
-        }
-        filterMadgwick.updateIMU(angular.x,angular.y,angular.z,acceleration.x,acceleration.y,acceleration.z);
-
-        float roll = filterMadgwick.getRoll();
-        float pitch = filterMadgwick.getPitch();
-        float heading = filterMadgwick.getYaw();
-        
-        Serial.printf("Roll: %.2f, Pitch: %.2f, Yaw: %.2f\n", 
-            roll,  // Roll
-            pitch,  // Pitch
-            heading); // Yaw
-        vTaskDelayUntil(&xLastWakeTime, xFrequency); // Consistently delay this task withouy yielding the CPU
-    }
-}
-      
 void testBMP(void *pvParameters)
 {
-    TickType_t xLastWakeTime = xTaskGetTickCount(); // Get current time in ticks
-    const TickType_t xFrequency = pdMS_TO_TICKS(10); // 100Hz/10ms
-    for(;;)
-    {
-        if(xSemaphoreTake(i2cMutex, portMAX_DELAY) == pdTRUE)
-        {
-            float temp = getTemperature();
-            float pressure = getPressure();
-            float baseline = getBaselinePressure();
-            float altitude = getRelativeAltitude();
-            xSemaphoreGive(i2cMutex);
-
-            Serial.printf("Temperature: %f *C, Pressure: %f Pa, Baseline Pressure: %f hPa, Relative Altitude: %f m\n", 
-                temp, pressure, baseline, altitude);
-        }
-        vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    for(;;) {
+        if(encodeGPS()){
+            Serial.print("Long=");
+            Serial.print(getLongitude(),8);
+            Serial.print(" Lat=");
+            Serial.print(getLatitude(),8);
+            Serial.print(" Time=");
+            getGPSTimeString();
+            Serial.print(" Sats=");
+            Serial.println(getSats(),8);  
+        } 
     }
-}
+};
 
 void setup()
 {
     Serial.begin(9600);
-    i2cMutex = xSemaphoreCreateMutex();
-    // Init sensors for tasks
-    initBMP();
-    initMPU6050();
-    filterMadgwick.begin(100); // Begin Madgwick filter at 100Hz
-    // Task Creation
-    // xTaskCreate(testTask, "Test Task", 2048, NULL, 1, &testTaskHandle);
+    initNEO6M();
     xTaskCreate(testBMP, "BMP280 Test Task", 4096, NULL, 1, &bmpTestHandle);
-    xTaskCreate(orientationLoop, "getOrientation", 4096, NULL, 1, &orientationLoopHandle);
 
     vTaskStartScheduler();
 }
