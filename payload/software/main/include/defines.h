fe488bb5c05209362db8d46dfc0a44557b77c355
John <110793201+NeverNotJohn@users.noreply.github.com>
2025-12-07 13:12:26 -0800

    Main task (#13)
    * semaphore mutex, madgwick
    
    can read both the mpu6050 and bmp280 at the "same " time.
    also implements the Madgwick filter for the mpu6050
    
    * Update main.cpp
    
    * NEO6M Intergration
    
    added
    functions to init and get values using the tinygpsplus library
    
    * changed serial port
    
    * cleanup n stuff
    
    * semaphore mutex, madgwick
    
    can read both the mpu6050 and bmp280 at the "same " time.
    also implements the Madgwick filter for the mpu6050
    
    * NEO6M Intergration
    
    added
    functions to init and get values using the tinygpsplus library
    
    * changed serial port
    
    * cleanup n stuff
    
    * Made changes to debug
    
    * Created Main task and added buzzer mutex
    
    * changed flight counters
    
    ---------
    
    Co-authored-by: Isaac L <isaac.lay25@gmail.com>
    Co-authored-by: Isaac Lay <isaaclay.25@gmail.com>

--- payload/software/main/include/defines.h
+++ payload/software/main/include/defines.h
@@ -7,7 +7,16 @@
 
 /************** FLAGS **************/
 #define ANNOY_CYAN          1
+ 
+// Preflight
+#define TRIGGER_FT          3       // 30
 
+// Inflight
+#define FLIGHT_TIMEOUT_S    900
+#define GROUND_COUNTER_MAX  250
+
+// Postflight
+#define POST_FLIGHT_TIME_S  60      // 900 = 15 minutes
 
 /************** PINS **************/
 #define LED_OUTPUT_PIN      30
@@ -37,5 +46,12 @@ typedef struct {
 //     double y;
 //     double z;
 // } Vector3D;
+
+typedef enum {
+    PRE_FLIGHT,
+    IN_FLIGHT,
+    POST_FLIGHT,
+    MISSION_END
+} MissionState;
 
 #endif // DEFINES_H
--- payload/software/main/src/main.cpp
+++ payload/software/main/src/main.cpp
@@ -5,6 +5,7 @@
 #include "bmp280.h"
 #include "i2c.h"
 #include "neo6m.h"
+#include <time.h>
 #include "timeUSLI.h"
 
 using namespace arduino;
@@ -13,7 +14,10 @@ using namespace arduino;
 TaskHandle_t blinkyHandle = NULL;
 TaskHandle_t loggerHandle = NULL;
 
-
+/************** GLOBAL VARS **************/
+MissionState currentState;
+SemaphoreHandle_t buzzerMutex;
+// FIXME Mutex for buzzer?
 
 /************** HELPER FUNCTIONS **************/
 // Beeps
@@ -22,15 +26,20 @@ void beep(int numBeeps, double onInterval_ms, double offInterval_ms)
 
     if (!ANNOY_CYAN) return;
 
-    const TickType_t offInterval = offInterval_ms / portTICK_PERIOD_MS; 
-    const TickType_t onInterval = onInterval_ms / portTICK_PERIOD_MS; 
-
-    for (int i = 0; i < numBeeps; i++)
+    if (xSemaphoreTake(buzzerMutex, portMAX_DELAY) == pdTRUE)
     {
-        digitalWrite(BUZZ_PIN, 1);
-        vTaskDelay(onInterval);
-        digitalWrite(BUZZ_PIN, 0);
-        vTaskDelay(offInterval);
+        const TickType_t offInterval = offInterval_ms / portTICK_PERIOD_MS; 
+        const TickType_t onInterval = onInterval_ms / portTICK_PERIOD_MS; 
+
+        for (int i = 0; i < numBeeps; i++)
+        {
+            digitalWrite(BUZZ_PIN, 1);
+            vTaskDelay(onInterval);
+            digitalWrite(BUZZ_PIN, 0);
+            vTaskDelay(offInterval);
+        }
+
+        xSemaphoreGive(buzzerMutex);
     }
 }
 
@@ -40,8 +49,8 @@ void beep(int numBeeps, double onInterval_ms, double offInterval_ms)
 // Sanity Check
 void blinkyTask(void *pvParameters)
 {
-    // Block for 1000 ms
-    const TickType_t xDelay = 1000 / portTICK_PERIOD_MS; 
+    // Block for 5000 ms
+    const TickType_t xDelay = 5000 / portTICK_PERIOD_MS; 
     bool ledStatus = 0;
 
     for (;;)
@@ -74,7 +83,76 @@ void loggerTask(void *pvParameters)
     }
 };
 
+// Main Task
+void mainTask(void *pvParameters)
+{
+    beep(3, 100, 50);
+    currentState = PRE_FLIGHT;
+    int preFlightCounter = 0;
 
+    // Preflight
+    while (preFlightCounter < 10)
+    {
+
+        // Look for calibration sequence
+
+        if (getAltitude_ft() > TRIGGER_FT)
+            preFlightCounter++;
+        else
+            preFlightCounter = 0;
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+
+    // Change States
+    currentState = IN_FLIGHT;
+    time_t liftOffTime_s = Teensy3Clock.get();
+    int groundCounter = 0;
+
+    // Outputs
+    beep(1, 1000, 0);
+    Serial.printf("LIFTOFF!\n");
+
+    // Inflight
+    // Triggers 25 secs after trigger
+    while (groundCounter < GROUND_COUNTER_MAX && 
+          ((Teensy3Clock.get() - liftOffTime_s) < FLIGHT_TIMEOUT_S))
+    {
+        if (getAltitude_ft() < TRIGGER_FT) 
+            groundCounter++;
+        else
+            groundCounter = 0;
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+
+    // Change States
+    currentState = POST_FLIGHT;
+    time_t touchDownTime_s = Teensy3Clock.get();
+
+    // Output
+    beep(5, 100, 10);
+    Serial.printf("TOUCHDOWN!\n");
+
+    // Post flight
+    // Cool Rover Stuff
+    while ((Teensy3Clock.get() - touchDownTime_s) < POST_FLIGHT_TIME_S)
+    {
+        // Do Rover Stuff
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
+
+    // Change States
+    currentState = MISSION_END;
+    time_t missionEndTime_s = Teensy3Clock.get();
+
+    // Output
+    beep(3, 1000, 500);
+    Serial.printf("YATA PLS SOIL!\n");
+
+    // End of Mission
+    for(;;) {}
+}
 
 void setup()
 {
@@ -88,6 +166,7 @@ void setup()
 
     // I/O INIT
     initI2C();
+    buzzerMutex = xSemaphoreCreateMutex();
 
     // DEVICE INIT
     initBMP();
@@ -99,6 +178,7 @@ void setup()
 
     // TASK CREATION
     xTaskCreate(blinkyTask, "Blinky Task", 4096, NULL, 1, &blinkyHandle);
+    xTaskCreate(mainTask, "Main Task", 4096, NULL, 1, &mainHandle);
     xTaskCreate(loggerTask, "Debug Task", 4096, NULL, 1, &loggerHandle);
 }
 
