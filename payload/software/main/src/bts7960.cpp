ac2d1c7e9d0b2d40c51f10f2f690b4df8fbdb7e4
Isaac <isaaclay.25@gmail.com>
2025-12-29 16:59:44 -0800

    init drill motor

---
+++ payload/software/main/src/bts7960.cpp
@@ -0,0 +1,32 @@
+/*****************************************
+ * 
+ * HiLetgo BTS7960 Motor Driver Source File
+ * 
+ *****************************************/
+
+#include "bts7960.h"
+#include "defines.h"
+
+using namespace std;
+
+
+void BTS7960Forward(int speed)
+{
+    analogWrite(BTS7960_LPWM_PIN, 0);
+    delayMicroseconds(100);
+    analogWrite(BTS7960_RPWM_PIN, speed);
+}
+
+
+void BTS7960Backward(int speed)
+{
+    analogWrite(BTS7960_RPWM_PIN, 0);
+    delayMicroseconds(100);
+    analogWrite(BTS7960_LPWM_PIN, speed);
+}
+
+void BTS7960Stop()
+{
+    analogWrite(BTS7960_RPWM_PIN, 0);
+    analogWrite(BTS7960_LPWM_PIN, 0);
+}
