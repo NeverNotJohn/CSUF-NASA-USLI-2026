07cec2f1ded8597ca645db3a7e4585dc97463ee5
Isaac Lay <51867136+Birb-jpg@users.noreply.github.com>
2026-01-31 14:48:36 -0800

    soil sensor yay (#18)

---
+++ payload/software/main/include/soilsensor.h
@@ -0,0 +1,26 @@
+/*****************************************
+ * 
+ * Hailsense 7-in-1 Soil Sensor Header File
+ * 
+ * Soil sensor used to read temperature, humidity, electircal conductivity, pH, and nitrogen, phosphorus, potassium
+ *****************************************/
+
+#ifndef SOIlSENSOR_H
+#define SOIlSENSOR_H
+
+/************** DEFINES **************/
+#define SOIlSENSOR_SERIAL        Serial1
+#define SOIlSENSOR_BAUD_RATE     4800
+
+/************** EXTERNAL FUNCTIONS **************/
+void initSoilSensor();
+bool readSoilSensor();
+float getSoilHumidity();
+float getSoilTemperature_c();
+float getSoilEC_us_cm();
+float getSoilpH();
+float getN_mg_kg();
+float getP_mg_kg();
+float getK_mg_kg();
+
+#endif // SOIlSENSOR_H
